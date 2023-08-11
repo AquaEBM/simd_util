668a6b9248129814e7f6373055d14747c88a532d
AquaEBM <AquaEBM@gmail.com>
2023-08-11 22:28:26 +0300

    improve smoothing api EVEN MORE

--- src/smoothing.rs
+++ src/smoothing.rs
@@ -2,46 +2,14 @@ use super::*;
 use simd_util::MAX_VECTOR_WIDTH;
 use math::pow;
 
-pub trait SIMDSmoother<T, const N: usize = MAX_VECTOR_WIDTH>
-where
-    LaneCount<N>: SupportedLaneCount,
-    T: SimdElement
-{
-    fn set_target(&mut self, target: Simd<T, N>, num_samples: usize);
-    fn set_instantly(&mut self, value: Simd<T, N>);
+pub trait Smoother {
+    type Value;
+
+    fn set_target(&mut self, target: Self::Value, num_samples: usize);
+    fn set_instantly(&mut self, value: Self::Value);
     fn tick(&mut self);
     fn tick_n(&mut self, n: u32);
-    fn get_current(&self) -> Simd<T, N>;
-}
-
-pub trait Smoother<T: SimdElement> {
-    fn set_target(&mut self, target: T, num_samples: usize);
-    fn set_instantly(&mut self, value: T);
-    fn tick(&mut self);
-    fn tick_n(&mut self, n: u32);
-    fn get_current(&self) -> T;
-}
-
-impl<U: SimdElement, T: SIMDSmoother<U, 1>> Smoother<U> for T {
-    fn tick(&mut self) {
-        self.tick();
-    }
-
-    fn set_target(&mut self, target: U, num_samples: usize) {
-        self.set_target(Simd::from_array([target]), num_samples);
-    }
-
-    fn set_instantly(&mut self, value: U) {
-        self.set_instantly(Simd::from_array([value]));
-    }
-
-    fn tick_n(&mut self, n: u32) {
-        self.tick_n(n);
-    }
-
-    fn get_current(&self) -> U {
-        self.get_current()[0]
-    }
+    fn get_current(&self) -> Self::Value;
 }
 
 #[derive(Clone, Copy)]
@@ -65,17 +33,19 @@ where
     }
 }
 
-impl<const N: usize> SIMDSmoother<f32, N> for LogSmoother<N>
+impl<const N: usize> Smoother for LogSmoother<N>
 where
     LaneCount<N>: SupportedLaneCount
 {
-    fn set_target(&mut self, target: Simd<f32, N>, num_samples: usize) {
+    type Value = Simd<f32, N>;
+
+    fn set_target(&mut self, target: Self::Value, num_samples: usize) {
         let base = target / self.value;
         let exp = Simd::splat(1. / num_samples as f32);
         self.factor = pow(base, exp);
     }
 
-    fn set_instantly(&mut self, value: Simd<f32, N>) {
+    fn set_instantly(&mut self, value: Self::Value) {
         self.value = value;
     }
 
@@ -88,7 +58,7 @@ where
         self.value *= pow(self.factor, Simd::splat(n as f32));
     }
 
-    fn get_current(&self) -> Simd<f32, N> {
+    fn get_current(&self) -> Self::Value {
         self.value
     }
 }
@@ -102,15 +72,17 @@ where
     value: Simd<f32, N>,
 }
 
-impl<const N: usize> SIMDSmoother<f32, N> for LinearSmoother<N>
+impl<const N: usize> Smoother for LinearSmoother<N>
 where
     LaneCount<N>: SupportedLaneCount
 {
-    fn set_target(&mut self, target: Simd<f32, N>, num_samples: usize) {
+    type Value = Simd<f32, N>;
+
+    fn set_target(&mut self, target: Self::Value, num_samples: usize) {
         self.increment = (target - self.value) / Simd::splat(num_samples as f32);
     }
 
-    fn set_instantly(&mut self, value: Simd<f32, N>) {
+    fn set_instantly(&mut self, value: Self::Value) {
         self.value = value;
     }
 
@@ -122,32 +94,28 @@ where
         self.value += self.increment * Simd::splat(n as f32);
     }
 
-    fn get_current(&self) -> Simd<f32, N> {
+    fn get_current(&self) -> Self::Value {
         self.value
     }
 }
 
-pub struct CachedTarget<T, U, const N: usize = MAX_VECTOR_WIDTH>
-where
-    T: SimdElement,
-    LaneCount<N>: SupportedLaneCount
-{
-    smoother: U,
-    target: Simd<T, N>,
+pub struct CachedTarget<T: Smoother> {
+    smoother: T,
+    target: T::Value,
 }
 
-impl<T, U, const N: usize> SIMDSmoother<T, N> for CachedTarget<T, U, N>
+impl<T: Smoother> Smoother for CachedTarget<T>
 where
-    T: SimdElement,
-    U: SIMDSmoother<T, N>,
-    LaneCount<N>: SupportedLaneCount,
+    T::Value: Clone
 {
-    fn set_target(&mut self, target: Simd<T, N>, num_samples: usize) {
-        self.target = target;
+    type Value = T::Value;
+
+    fn set_target(&mut self, target: Self::Value, num_samples: usize) {
+        self.target = target.clone();
         self.smoother.set_target(target, num_samples);
     }
 
-    fn set_instantly(&mut self, value: Simd<T, N>) {
+    fn set_instantly(&mut self, value: Self::Value) {
         self.smoother.set_instantly(value);
     }
 
@@ -159,17 +127,16 @@ where
         self.smoother.tick_n(n)
     }
 
-    fn get_current(&self) -> Simd<T, N> {
+    fn get_current(&self) -> Self::Value {
         self.smoother.get_current()
     }
 }
 
-impl<T, U, const N: usize> CachedTarget<T, U, N>
+impl<T: Smoother> CachedTarget<T>
 where
-    T: SimdElement,
-    LaneCount<N>: SupportedLaneCount,
+    T::Value: Clone
 {
-    pub fn get_target(&self) -> Simd<T, N> {
-        self.target
+    pub fn get_target(&self) -> T::Value {
+        self.target.clone()
     }
 }
