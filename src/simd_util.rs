328d97ad35e402b409fe29ce98bf53d2d906e005
unknown <AquaEBM@gmail.com>
2023-12-24 19:28:14 +0100

    use raw pointers for the gather functions since the length is useless

--- src/simd_util.rs
+++ src/simd_util.rs
@@ -1,13 +1,11 @@
 use super::simd;
-use simd::{Mask as StdMask, *};
-use std::mem::size_of;
+use simd::{Simd, SimdElement, LaneCount, SupportedLaneCount};
+
+use std::mem;
 use cfg_if::cfg_if;
 
 #[cfg(any(target_feature = "avx512f", target_feature = "avx2"))]
-use {
-    std::arch::x86_64::*,
-    std::mem::transmute,
-};
+use std::arch::x86_64::*;
 
 pub const MAX_VECTOR_WIDTH: usize = {
     if cfg!(any(target_feature = "avx512f")) {
@@ -21,11 +19,11 @@ pub const MAX_VECTOR_WIDTH: usize = {
     }
 };
 
-pub const FLOATS_PER_VECTOR: usize = MAX_VECTOR_WIDTH / size_of::<f32>();
+pub const FLOATS_PER_VECTOR: usize = MAX_VECTOR_WIDTH / mem::size_of::<f32>();
 
 pub type Float = Simd<f32, FLOATS_PER_VECTOR>;
 pub type UInt = Simd<u32, FLOATS_PER_VECTOR>;
-pub type Mask = StdMask<i32, FLOATS_PER_VECTOR>;
+pub type Mask = simd::Mask<i32, FLOATS_PER_VECTOR>;
 
 /// Convenience function on simd types when specialized functions aren't
 /// available in the standard library, hoping autovectorization compiles this
@@ -53,14 +51,14 @@ where
 
 // We're using intrinsics for now because u32 gathers aren't in core::simd (yet?)
 #[inline]
-pub unsafe fn gather_select_unchecked(slice: &[f32], index: UInt, mask: Mask, or: Float) -> Float {
+pub unsafe fn gather_select_unchecked(ptr: *const f32, index: UInt, mask: Mask, or: Float) -> Float {
 
     cfg_if! {
 
         if #[cfg(target_feature = "avx512f")] {
 
             #[cfg(feature = "non_std_simd")]
-            let bitmask = transmute(mask);
+            let bitmask = mem::transmute(mask);
             #[cfg(not(feature = "non_std_simd"))]
             let bitmask = mask.to_bitmask();
 
@@ -68,7 +66,7 @@ pub unsafe fn gather_select_unchecked(slice: &[f32], index: UInt, mask: Mask, or
                 or.into(),
                 bitmask,
                 index.into(),
-                slice.as_ptr().cast(),
+                ptr.cast(),
                 4,
             ).into()
 
@@ -76,33 +74,40 @@ pub unsafe fn gather_select_unchecked(slice: &[f32], index: UInt, mask: Mask, or
 
             _mm256_mask_i32gather_ps(
                 or.into(),
-                slice.as_ptr(),
+                ptr,
                 index.into(),
-                transmute(mask), // Why is this __m256, not __m256i? I don't know
+                mem::transmute(mask), // Why is this __m256, not __m256i? I don't know
                 4
             ).into()
 
         } else {
-            Simd::gather_select_unchecked(slice, mask.cast(), index.cast(), or)
+            let slice = core::slice::from_raw_parts(ptr, 0);
+            Simd::gather_select_unchecked(slice, mask.cast(), simd::SimdUint::cast(index), or)
         }
     }
 }
 
 #[inline]
-pub unsafe fn gather_unchecked(slice: &[f32], index: UInt) -> Float {
+pub unsafe fn gather_unchecked(ptr: *const f32, index: UInt) -> Float {
 
     cfg_if! {
     
         if #[cfg(target_feature = "avx512f")] {
 
-            _mm512_i32gather_ps(index.into(), slice.as_ptr().cast(), 4).into()
+            _mm512_i32gather_ps(index.into(), ptr.cast(), 4).into()
         
         } else if #[cfg(target_feature = "avx2")] {
         
-            _mm256_i32gather_ps(slice.as_ptr(), index.into(), 4).into()
+            _mm256_i32gather_ps(ptr, index.into(), 4).into()
         
         } else {
-            Simd::gather_or_default(slice, index.cast())
+            let slice = core::slice::from_raw_parts(ptr, 0);
+            Simd::gather_select_unchecked(
+                slice,
+                simd::Mask::splat(true),
+                simd::SimdUint::cast(index),
+                Float::splat(0.)
+            )
         }
     }
 }
